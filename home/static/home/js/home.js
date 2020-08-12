f24ea0d1ef60f8f367f6f01bb6bb54b612b05625
Siddharth Jha <SidJha03@gmail.com>
2020-08-12 19:04:51 -0400

    fixed counters for mobile

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,37 +1,67 @@
-$(window).scroll(function () {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
+if (window.innerWidth > 560) {
+    $(window).scroll(function () {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
 
-    var elemTop = $('section.counters').offset().top;
-    var elemBottom = elemTop + $('section.counters').height();
+        var elemTop = $('section.counters').offset().top;
+        var elemBottom = elemTop + $('section.counters').height();
 
-    if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
-        const counters = document.querySelectorAll('.counter');
-        const speed = 500; // The lower the slower
+        if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+            const counters = document.querySelectorAll('.counter');
+            const speed = 400; // The lower the slower
 
-        counters.forEach(counter => {
-            const updateCount = () => {
-                const target = +counter.getAttribute('data-target');
-                const count = +counter.innerText;
+            counters.forEach(counter => {
+                const updateCount = () => {
+                    const target = +counter.getAttribute('data-target');
+                    const count = +counter.innerText;
 
-                // Lower inc to slow and higher to slow
-                const inc = target / speed;
+                    // Lower inc to slow and higher to slow
+                    const inc = target / speed;
 
-                // console.log(inc);
-                // console.log(count);
+                    // console.log(inc);
+                    // console.log(count);
 
-                // Check if target is reached
-                if (count < target) {
-                    // Add inc to count and output in counter
-                    counter.innerText = count + inc;
-                    // Call function every ms
-                    setTimeout(updateCount, 1);
-                } else {
-                    counter.innerText = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
-                }
-            };
+                    // Check if target is reached
+                    if (count < target) {
+                        // Add inc to count and output in counter
+                        counter.innerText = count + inc;
+                        // Call function every ms
+                        setTimeout(updateCount, 1);
+                    } else {
+                        counter.innerText = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+                    }
+                };
 
-            updateCount();
-        });
-    }
-});
+                updateCount();
+            });
+        }
+    });
+} else {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 400; // The lower the slower
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            // Lower inc to slow and higher to slow
+            const inc = target / speed;
+
+            // console.log(inc);
+            // console.log(count);
+
+            // Check if target is reached
+            if (count < target) {
+                // Add inc to count and output in counter
+                counter.innerText = count + inc;
+                // Call function every ms
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+            }
+        };
+
+        updateCount();
+    });
+}
