4827adba5e61aeb8e1dc4092e8188ca5ead374fd
Siddharth Jha <SidJha03@gmail.com>
2020-08-12 18:58:20 -0400

    Made counters start on scroll

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,27 +1,37 @@
-const counters = document.querySelectorAll('.counter');
-const speed = 200; // The lower the slower
+$(window).scroll(function () {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
 
-counters.forEach(counter => {
-    const updateCount = () => {
-        const target = +counter.getAttribute('data-target');
-        const count = +counter.innerText;
+    var elemTop = $('section.counters').offset().top;
+    var elemBottom = elemTop + $('section.counters').height();
 
-        // Lower inc to slow and higher to slow
-        const inc = target / speed;
+    if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+        const counters = document.querySelectorAll('.counter');
+        const speed = 500; // The lower the slower
 
-        // console.log(inc);
-        // console.log(count);
+        counters.forEach(counter => {
+            const updateCount = () => {
+                const target = +counter.getAttribute('data-target');
+                const count = +counter.innerText;
 
-        // Check if target is reached
-        if (count < target) {
-            // Add inc to count and output in counter
-            counter.innerText = count + inc;
-            // Call function every ms
-            setTimeout(updateCount, 1);
-        } else {
-            counter.innerText = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
-        }
-    };
+                // Lower inc to slow and higher to slow
+                const inc = target / speed;
 
-    updateCount();
+                // console.log(inc);
+                // console.log(count);
+
+                // Check if target is reached
+                if (count < target) {
+                    // Add inc to count and output in counter
+                    counter.innerText = count + inc;
+                    // Call function every ms
+                    setTimeout(updateCount, 1);
+                } else {
+                    counter.innerText = target.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
+                }
+            };
+
+            updateCount();
+        });
+    }
 });
